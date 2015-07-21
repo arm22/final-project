b4d0a3d003d1d1381544b8cf27f30c19a26832c3
Austin Meyers <meyersaustin22@gmail.com>
2015-07-20 19:16:00 -0700

    Added clock

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-var data
+var data;
 
-var baseURL = "http://api.openweathermap.org/data/2.5/forecast/city?id=5809844&mode=json&units=imperial"
+var baseURL = "http://api.openweathermap.org/data/2.5/forecast/city?id=5809844&mode=json&units=imperial";
 
-
-var apiKey = "&APPID=0c02cd3e6c47bafe3bc17f4b7c38ef6c"
+var apiKey = "&APPID=0c02cd3e6c47bafe3bc17f4b7c38ef6c";
 
 var myApp = angular.module('myApp', ['ngRoute'])
 
@@ -32,7 +31,7 @@ var myApp = angular.module('myApp', ['ngRoute'])
 .controller('HomeController', function($scope, $http){
 
   $scope.getWeather = function() {
-      $http.get(baseURL + apiKey).success(function(response){
+      $http.get(baseURL + apiKey).success(function(response) {
         data = $scope.weather = response.list;
         console.log(data);
         //First day at 12
@@ -47,28 +46,27 @@ var myApp = angular.module('myApp', ['ngRoute'])
         $scope.day5 = data[35];
       })
     }
-
     angular.element(document).ready(function () {
         $scope.getWeather();
     });
 })
 
 // About page controller
-.controller('CalenderController', function($scope){
+.controller('CalenderController', function($scope) {
 
 
-  $scope.about = "Here's some information about this page."
+  //$scope.about = "Here's some information about this page."
 })
 
 // Content controller
-.controller('RemindersController', function($scope){
+.controller('RemindersController', function($scope) {
 
 
   //$scope.url = "http://conference.unavsa.org/wp-content/uploads/2015/06/SEA-pic.jpg"
 })
 
 //Tooltip functionality
-myApp.directive('tooltip', function(){
+myApp.directive('tooltip', function() {
     return {
         restrict: 'A',
         link: function(scope, element, attrs){
@@ -82,6 +80,25 @@ myApp.directive('tooltip', function(){
         }
     };
 });
+
+myApp.directive('clock', ['dateFilter', '$timeout', function(dateFilter, $timeout){
+    return {
+        restrict: 'E',
+        scope: {
+            format: '@'
+        },
+        link: function(scope, element, attrs){
+            var updateTime = function(){
+                var now = Date.now();
+                
+                element.html(dateFilter(now, scope.format));
+                $timeout(updateTime, now % 1000);
+            };
+            
+            updateTime();
+        }
+    };
+}]);
 
 //Filter string as int
 myApp.filter('ceil', function() {
